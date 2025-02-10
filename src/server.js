383ed3711df177cea52c6b1c70a4bda1c7f94c20
rihan-a <a.rihan@live.com>
2025-02-10 17:06:03 +0100

    intial project setup

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const vehiclesRoutes = require('./src/routes/vehicles.routes');
+
+const app = express();
+const PORT = 3005;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/vehicles', vehiclesRoutes);
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
