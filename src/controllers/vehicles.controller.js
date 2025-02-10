d18950abd78d6a76907793bdb81d4110c4d4e8b3
rihan-a <a.rihan@live.com>
2025-02-10 22:00:13 +0100

    add logic for create, update and delete vehicles

--- src/controllers/vehicles.controller.js
+++ src/controllers/vehicles.controller.js
@@ -1,5 +1,7 @@
 const vehiclesService = require('../services/vehicles.service');
 
+
+// GET all vehicles
 const getAllVehicles = async (req, res) => {
     try {
         const cars = await vehiclesService.getAllVehicles();
@@ -9,6 +11,7 @@ const getAllVehicles = async (req, res) => {
     }
 };
 
+// Search vehicles
 const searchVehicles = async (req, res) => {
     try {
         const { q, brand, minPrice, maxPrice } = req.query;
@@ -19,4 +22,37 @@ const searchVehicles = async (req, res) => {
     }
 };
 
-module.exports = { getAllVehicles, searchVehicles };
+// CREATE a new vehicle
+const createVehicle = async (req, res) => {
+    try {
+        const newVehicle = await vehiclesService.createVehicle(req.body);
+        res.status(201).json(newVehicle);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+// UPDATE a vehicle
+const updateVehicle = async (req, res) => {
+    try {
+        const updatedVehicle = await vehiclesService.updateVehicle(req.params.id, req.body);
+        res.json(updatedVehicle);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+// DELETE a vehicle
+const deleteVehicle = async (req, res) => {
+    try {
+        await vehiclesService.deleteVehicle(req.params.id);
+        res.status(204).send();
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+
+
+
+module.exports = { getAllVehicles, searchVehicles, createVehicle, updateVehicle, deleteVehicle };
